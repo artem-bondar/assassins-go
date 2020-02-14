9fd8dedaf328319d45733ce7d441ede68c00d730
Artem Bondar <artem.bondar.dev@gmail.com>
2020-02-14 18:56:19 +0300

    Add iTweenExample for testing

---
+++ Assets/Scripts/iTweenExample.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class iTweenExample : MonoBehaviour
+{
+    private void Start()
+    {
+        iTween.RotateTo(gameObject, iTween.Hash(
+            "y", 90f,
+            "delay", 1f,
+            "time", 2f,
+            "easetype",
+            iTween.EaseType.easeInOutExpo));
+    }
+}
