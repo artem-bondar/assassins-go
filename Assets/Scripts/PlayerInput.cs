701037d2cbe0c64dbf189fed7f7da723d3d6bd1e
Artem Bondar <artem.bondar.dev@gmail.com>
2020-02-20 18:25:21 +0300

    Create PlayerInput class

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float h;
+    public float v;
+
+    public bool inputEnabled = false;
+
+    public void GetKeyInput()
+    {
+        if (inputEnabled)
+        {
+            h = Input.GetAxisRaw("Horizontal");
+            v = Input.GetAxisRaw("Vertical");
+        }
+    }
+}
