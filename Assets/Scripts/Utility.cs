677211c638aef5b9c3a829ecb27878b164062d56
Artem Bondar <artem.bondar.dev@gmail.com>
2020-03-01 01:10:22 +0300

    Create Utility class

---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Utility
+{
+    public static Vector3 Vector3Round(Vector3 inputVector) =>
+        new Vector3(Mathf.Round(inputVector.x), Mathf.Round(inputVector.y), Mathf.Round(inputVector.x));
+
+    public static Vector2 Vector2Round(Vector2 inputVector) =>
+        new Vector2(Mathf.Round(inputVector.x), Mathf.Round(inputVector.y));
+}
