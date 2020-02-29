f20607ca59cec673cd7cea7f722d18aafe20a752
Artem Bondar <artem.bondar.dev@gmail.com>
2020-02-29 21:40:13 +0300

    Create node game object

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public GameObject geometry;
+
+    public float scaleTime = 0.3f;
+    public float delay = 1f;
+
+    public iTween.EaseType easeType = iTween.EaseType.easeInExpo;
+
+    public bool autoRun = false;
+
+    private void Start()
+    {
+        if (geometry != null)
+        {
+            geometry.transform.localScale = Vector3.zero;
+        }
+
+        if (autoRun)
+        {
+            ShowGeometry();
+        }
+    }
+
+    public void ShowGeometry()
+    {
+        if (geometry != null)
+        {
+            iTween.ScaleTo(geometry, iTween.Hash(
+                "time", scaleTime,
+                "scale", Vector3.one,
+                "easetype", easeType,
+                "delay", delay
+            ));
+        }
+    }
+}
