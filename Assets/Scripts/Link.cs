fcdabbe43e4c0231ec1b30a6a5b322b072937dfd
Artem Bondar <artem.bondar.dev@gmail.com>
2020-03-23 02:28:57 +0300

    Create Link class with initialize methodes

---
+++ Assets/Scripts/Link.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+    public float borderWidth = 0.07f;
+    public float lineThickness = 0.3f;
+
+    public float scaleTime = 0.3f;
+    public float delay = 0.1f;
+
+    public iTween.EaseType easeType = iTween.EaseType.easeInOutSine;
+
+    private void Start() => DrawLink(new Vector3(4f, 0f, 0f), new Vector3(4f, 0f, 2f));
+
+    public void DrawLink(Vector3 startPosition, Vector3 endPosition)
+    {
+        transform.localScale = new Vector3(lineThickness, 1f, 0f);
+
+        Vector3 directionVector = endPosition - startPosition;
+
+        float zScale = directionVector.magnitude - borderWidth * 2f;
+
+        Vector3 newScale = new Vector3(lineThickness, 1f, zScale);
+
+        transform.rotation = Quaternion.LookRotation(directionVector);
+        transform.position = startPosition + (transform.forward * borderWidth);
+
+        iTween.ScaleTo(gameObject, iTween.Hash(
+            "time", scaleTime,
+            "scale", newScale,
+            "easetype", easeType,
+            "delay", delay
+        ));
+    }
+}
